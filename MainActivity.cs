b7bd3db6cc19c2c465dbdf18d0119301e732901a
qwe <wednesdaymydudes@protonmail.com>
2020-12-02 10:20:09 -0500

    Major Directory Restructure- Merge Areebahs Code

--- MainActivity.cs
+++ MainActivity.cs
@@ -30,63 +30,13 @@ namespace UTLP
                 var ActivityStartIntent = new Intent(this, typeof(FuelAddActivity));
                 StartActivity(ActivityStartIntent);
             };
-        }
-    }
-    
-    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity
-    {
-        protected override void OnCreate(Bundle savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            SetContentView(Resource.Layout.activity_main);
 
-            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
-            SetSupportActionBar(toolbar);
-
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
-            fab.Click += FabOnClick;
-            Button sendbutton = FindViewById<Button>(Resource.Id.button_send);
-            sendbutton.Click += SendMessage;
-        }
-
-        public override bool OnCreateOptionsMenu(IMenu menu)
-        {
-            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
-            return true;
-        }
-
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            int id = item.ItemId;
-            if (id == Resource.Id.action_settings)
+            var Button3 = FindViewById<Button>(Resource.Id.TestButton3);
+            Button3.Click += (Sender, Args) =>
             {
-                return true;
-            }
-
-            return base.OnOptionsItemSelected(item);
-        }
-
-        private void SendMessage (object sender, EventArgs eventArgs)
-        {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-
-        }
-        private void FabOnClick(object sender, EventArgs eventArgs)
-        {
-            View view = (View)sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
-        }
-
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
-
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+                var ActivityStartIntent = new Intent(this, typeof(CarAddActivity));
+                StartActivity(ActivityStartIntent);
+            };
         }
     }
 }
